ab2cca6e86238eea104a2cb02de21344a2872462
Marcio Machado <marcio_mach@hotmail.com>
2020-09-09 13:08:03 -0300

    Incluído configuração para documentação no swagger.

--- OrderPizza/OrderPizza.API/Startup.cs
+++ OrderPizza/OrderPizza.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using OrderPizza.Data.Contexts;
 using OrderPizza.Data.Repositories;
 
@@ -33,6 +34,22 @@ namespace OrderPizza.API
                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddSwaggerGen(c => {
+
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "Pedido de Pizzas",
+                        Version = "v1",
+                        Description = "API REST criada com o ASP.NET Core 3.1 para Pedido de Pizzas",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "OrderPizza no GitHub de Marcio Machado",
+                            Url = new Uri("https://github.com/machadomarcio/OrderPizza")
+                        }
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +61,11 @@ namespace OrderPizza.API
             }
 
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pedido de Pizzas V1");
+            });
 
             app.UseRouting();
 
